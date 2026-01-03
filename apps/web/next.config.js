869fda6c9c4b7e45ec053ee71c03493e14adee49
Leon <leontang27@icloud.com>
2026-01-03 17:48:31 -0500

    commit

---
+++ apps/web/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['@codingcrazy/engine', '@codingcrazy/shared'],
+};
+
+module.exports = nextConfig;
